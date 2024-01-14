750605bfab71634e0a391339fd8d22a41b0cba08
Shubham Sah <shubham.lal.sah@gmail.com>
2024-01-14 15:57:49 -0600

    fix

--- Movetest.py
+++ Movetest.py
@@ -114,7 +114,7 @@ while stop == 0:
         servo12angle = servo12.get_physical_angle()
         print("servo12angle: ", servo12angle )
 
-        time.sleep(1)
+        time.sleep(3)
 
         stop = joy1.get_button(5)
 
