49563b24d5cc69e8e031368ee7febee2539bc796
Shubham Sah <shubham.lal.sah@gmail.com>
2024-01-26 21:31:03 -0600

    changed pos

--- rest.py
+++ rest.py
@@ -9,7 +9,7 @@ def standing_position(FL_TIBIA,FL_FEMUR,FL_HIP,FR_TIBIA,FR_FEMUR,FR_HIP,BR_TIBIA
     FR_TIBIA.move(120,1000,False,False)
     FR_FEMUR.move(115,1000,False,False)
     FR_HIP.move(124,1000,False,False)
-    BR_TIBIA.move(130,1000,False,False)
+    BR_TIBIA.move(110,1000,False,False)
     BR_FEMUR.move(152,1000,False,False)
     BR_HIP.move(115,1000,False,False)
     BL_TIBIA.move(141,1000,False,False)
