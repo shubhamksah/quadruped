1850529966c306425d1e9fd96ae29d23ae4bed1c
Shubham Sah <shubham.lal.sah@gmail.com>
2024-01-28 08:16:54 -0600

    fixed standing + feedback

--- Controllertest.py
+++ Controllertest.py
@@ -96,21 +96,25 @@ try:
                 BL_TIBIA.move(91.52,1000,False,False)
                 BL_FEMUR.move(153.12,1000,False,False)
                 BL_HIP.move(141.84,1000,False,False)
+
+                print("Resting")
             
             if joy1.get_button(1) == 1:
-                FL_TIBIA.move(85.52,1000,False,False)
-                FL_FEMUR.move(169.92,1000,False,False)
-                FL_HIP.move(121.92,1000,False,False)
-                FR_TIBIA.move(161.44,1000,False,False)
-                FR_FEMUR.move(84.72,1000,False,False)
-                FR_HIP.move(124.08,1000,False,False)
-                BR_TIBIA.move(154.24,1000,False,False)
-                BR_FEMUR.move(122.4,1000,False,False)
-                BR_HIP.move(110.88,1000,False,False)
-                BL_TIBIA.move(91.52,1000,False,False)
-                BL_FEMUR.move(153.12,1000,False,False)
-                BL_HIP.move(141.84,1000,False,False)
-            
+                FL_TIBIA.move(125,1000,False,False)
+                FL_FEMUR.move(143,1000,False,False)
+                FL_HIP.move(121.5,1000,False,False)
+                FR_TIBIA.move(120,1000,False,False)
+                FR_FEMUR.move(115,1000,False,False)
+                FR_HIP.move(124,1000,False,False)
+                BR_TIBIA.move(105,1000,False,False)
+                BR_FEMUR.move(152,1000,False,False)
+                BR_HIP.move(115,1000,False,False)
+                BL_TIBIA.move(141,1000,False,False)
+                BL_FEMUR.move(126,1000,False,False)
+                BL_HIP.move(141,1000,False,False)
+
+                print("Standing")
+                        
             t = joy1.get_button(0)
 
         finally:
