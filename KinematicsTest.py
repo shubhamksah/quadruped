81edec1a533f71dd48d89f1b1ad3112096be1afb
Shubham Sah <shubham.lal.sah@gmail.com>
2024-02-16 17:38:02 -0600

    f

--- KinematicsTest.py
+++ KinematicsTest.py
@@ -22,13 +22,13 @@ try:
     
     a = 1
     while (a < 10):
-        FL_TIBIA.move(125,movetime,False,False)
+        FL_TIBIA.move(123,movetime,False,False)
         FL_FEMUR.move(190,movetime,False,False)
         FL_HIP.move(121.5,movetime,False,False)
         FR_TIBIA.move(103,movetime,False,False)
         FR_FEMUR.move(124,movetime,False,False)
         FR_HIP.move(124,movetime,False,False)
-        BR_TIBIA.move(116,movetime,False,False)
+        BR_TIBIA.move(118,movetime,False,False)
         BR_FEMUR.move(105,movetime,False,False)
         BR_HIP.move(115,movetime,False,False)
         BL_TIBIA.move(153,movetime,False,False)
@@ -37,13 +37,13 @@ try:
 
         time.sleep(3)
 
-        FL_TIBIA.move(125,movetime,False,False)
+        FL_TIBIA.move(123,movetime,False,False)
         FL_FEMUR.move(190,movetime,False,False)
         FL_HIP.move(121.5,movetime,False,False)
         FR_TIBIA.move(92,movetime,False,False)
         FR_FEMUR.move(69,movetime,False,False)
         FR_HIP.move(124,movetime,False,False)
-        BR_TIBIA.move(116,movetime,False,False)
+        BR_TIBIA.move(118,movetime,False,False)
         BR_FEMUR.move(105,movetime,False,False)
         BR_HIP.move(115,movetime,False,False)
         BL_TIBIA.move(164,movetime,False,False)
@@ -52,13 +52,13 @@ try:
 
         time.sleep(3)
 
-        FL_TIBIA.move(149,movetime,False,False)
+        FL_TIBIA.move(147,movetime,False,False)
         FL_FEMUR.move(134,movetime,False,False)
         FL_HIP.move(121.5,movetime,False,False)
         FR_TIBIA.move(127,movetime,False,False)
         FR_FEMUR.move(68,movetime,False,False)
         FR_HIP.move(124,movetime,False,False)
-        BR_TIBIA.move(92,movetime,False,False)
+        BR_TIBIA.move(94,movetime,False,False)
         BR_FEMUR.move(161,movetime,False,False)
         BR_HIP.move(115,movetime,False,False)
         BL_TIBIA.move(129,movetime,False,False)
@@ -68,13 +68,13 @@ try:
 
         time.sleep(3)
 
-        FL_TIBIA.move(164,movetime,False,False)
+        FL_TIBIA.move(162,movetime,False,False)
         FL_FEMUR.move(181.6,movetime,False,False)
         FL_HIP.move(121.5,movetime,False,False)
         FR_TIBIA.move(127,movetime,False,False)
         FR_FEMUR.move(68,movetime,False,False)
         FR_HIP.move(124,movetime,False,False)
-        BR_TIBIA.move(76.5,movetime,False,False)
+        BR_TIBIA.move(78.5,movetime,False,False)
         BR_FEMUR.move(113.4,movetime,False,False)
         BR_HIP.move(115,movetime,False,False)
         BL_TIBIA.move(129,movetime,False,False)
