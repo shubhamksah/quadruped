6eb8a4dbaa3ef5625641871dd6e98714e23e66cd
Shubham Sah <shubham.lal.sah@gmail.com>
2024-02-16 16:42:14 -0600

    f

--- ControlRobot.py
+++ ControlRobot.py
@@ -4,16 +4,16 @@ import time
 
 def standing_position(FL_TIBIA,FL_FEMUR,FL_HIP,FR_TIBIA,FR_FEMUR,FR_HIP,BR_TIBIA,BR_FEMUR,BR_HIP,BL_TIBIA,BL_FEMUR,BL_HIP):
 
-    FL_TIBIA.move(125,1000,False,False)
+    FL_TIBIA.move(142,1000,False,False)
     FL_FEMUR.move(143,1000,False,False)
     FL_HIP.move(121.5,1000,False,False)
-    FR_TIBIA.move(120,1000,False,False)
+    FR_TIBIA.move(110,1000,False,False)
     FR_FEMUR.move(115,1000,False,False)
     FR_HIP.move(124,1000,False,False)
-    BR_TIBIA.move(105,1000,False,False)
+    BR_TIBIA.move(99,1000,False,False)
     BR_FEMUR.move(152,1000,False,False)
     BR_HIP.move(115,1000,False,False)
-    BL_TIBIA.move(141,1000,False,False)
+    BL_TIBIA.move(146,1000,False,False)
     BL_FEMUR.move(126,1000,False,False)
     BL_HIP.move(141,1000,False,False)
 
