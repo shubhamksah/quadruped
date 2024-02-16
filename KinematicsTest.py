aaa94765890c22a9bcc3f35a78791a19e7af98be
Shubham Sah <shubham.lal.sah@gmail.com>
2024-02-16 16:12:47 -0600

    fix

--- KinematicsTest.py
+++ KinematicsTest.py
@@ -34,18 +34,18 @@ try:
 
     time.sleep(1)
 
-    FL_TIBIA.move(143,1000,False,False)
-    FL_FEMUR.move(189,1000,False,False)
-    FL_HIP.move(121.5,1000,False,False)
-    FR_TIBIA.move(120,1000,False,False)
-    FR_FEMUR.move(115,1000,False,False)
-    FR_HIP.move(124,1000,False,False)
-    BR_TIBIA.move(105,1000,False,False)
-    BR_FEMUR.move(152,1000,False,False)
-    BR_HIP.move(115,1000,False,False)
-    BL_TIBIA.move(141,1000,False,False)
-    BL_FEMUR.move(126,1000,False,False)
-    BL_HIP.move(141,1000,False,False)
+    # FL_TIBIA.move(143,1000,False,False)
+    # FL_FEMUR.move(189,1000,False,False)
+    # FL_HIP.move(121.5,1000,False,False)
+    # FR_TIBIA.move(120,1000,False,False)
+    # FR_FEMUR.move(115,1000,False,False)
+    # FR_HIP.move(124,1000,False,False)
+    # BR_TIBIA.move(105,1000,False,False)
+    # BR_FEMUR.move(152,1000,False,False)
+    # BR_HIP.move(115,1000,False,False)
+    # BL_TIBIA.move(141,1000,False,False)
+    # BL_FEMUR.move(126,1000,False,False)
+    # BL_HIP.move(141,1000,False,False)
 
 
 
