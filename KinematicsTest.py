c169a6b8520b22baadfd50181c3d0da6062a01bc
Shubham Sah <shubham.lal.sah@gmail.com>
2024-02-16 15:59:00 -0600

    fix

--- KinematicsTest.py
+++ KinematicsTest.py
@@ -2,36 +2,6 @@ from pylx16a.lx16a import *
 import time
 import algorithm
 
-def standing_position(FL_TIBIA,FL_FEMUR,FL_HIP,FR_TIBIA,FR_FEMUR,FR_HIP,BR_TIBIA,BR_FEMUR,BR_HIP,BL_TIBIA,BL_FEMUR,BL_HIP):
-
-    FL_TIBIA.move(125,1000,False,False)
-    FL_FEMUR.move(143,1000,False,False)
-    FL_HIP.move(121.5,1000,False,False)
-    FR_TIBIA.move(120,1000,False,False)
-    FR_FEMUR.move(115,1000,False,False)
-    FR_HIP.move(124,1000,False,False)
-    BR_TIBIA.move(105,1000,False,False)
-    BR_FEMUR.move(152,1000,False,False)
-    BR_HIP.move(115,1000,False,False)
-    BL_TIBIA.move(141,1000,False,False)
-    BL_FEMUR.move(126,1000,False,False)
-    BL_HIP.move(141,1000,False,False)
-
-def resting_position(FL_TIBIA,FL_FEMUR,FL_HIP,FR_TIBIA,FR_FEMUR,FR_HIP,BR_TIBIA,BR_FEMUR,BR_HIP,BL_TIBIA,BL_FEMUR,BL_HIP):
-    
-    FL_TIBIA.move(85.52,1000,False,False)
-    FL_FEMUR.move(169.92,1000,False,False)
-    FL_HIP.move(121.92,1000,False,False)
-    FR_TIBIA.move(161.44,1000,False,False)
-    FR_FEMUR.move(84.72,1000,False,False)
-    FR_HIP.move(124.08,1000,False,False)
-    BR_TIBIA.move(154.24,1000,False,False)
-    BR_FEMUR.move(122.4,1000,False,False)
-    BR_HIP.move(110.88,1000,False,False)
-    BL_TIBIA.move(91.52,1000,False,False)
-    BL_FEMUR.move(153.12,1000,False,False)
-    BL_HIP.move(141.84,1000,False,False)
-
 LX16A.initialize("/dev/ttyUSB0", 0.1)
 
 try:
@@ -49,8 +19,8 @@ try:
     BL_FEMUR = LX16A(11)
     BL_HIP = LX16A(12)
 
-    FL_TIBIA.move(125,1000,False,False)
-    FL_FEMUR.move(143,1000,False,False)
+    FL_TIBIA.move(104.644,1000,False,False)
+    FL_FEMUR.move(185.773,1000,False,False)
     FL_HIP.move(121.5,1000,False,False)
     FR_TIBIA.move(120,1000,False,False)
     FR_FEMUR.move(115,1000,False,False)
