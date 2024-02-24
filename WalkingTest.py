2552b85a3fa4e703651dc161e25caadc07dbb568
Shubham Sah <shubham.lal.sah@gmail.com>
2024-02-24 17:48:47 -0600

    f

--- WalkingTest.py
+++ WalkingTest.py
@@ -39,14 +39,14 @@ try:
     FL_TIBIA.move(135,movetime,False,False)
     FL_FEMUR.move(134,movetime,False,False)
     FL_HIP.move(121.5,movetime,False,False)
-    FR_TIBIA.move(77,1000,False,False)
-    FR_FEMUR.move(69,1000,False,False)
+    FR_TIBIA.move(92,1000,False,False)
+    FR_FEMUR.move(84,1000,False,False)
     FR_HIP.move(124,1000,False,False)
     BR_TIBIA.move(106,movetime,False,False)
     BR_FEMUR.move(161,movetime,False,False)
     BR_HIP.move(115,movetime,False,False)
-    BL_TIBIA.move(179,1000,False,False)
-    BL_FEMUR.move(172,1000,False,False)
+    BL_TIBIA.move(164,1000,False,False)
+    BL_FEMUR.move(157,1000,False,False)
     BL_HIP.move(141,1000,False,False)
 
 
