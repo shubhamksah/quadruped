8ced3a6c2aed1721986ce791faaf7cd5ea88f874
Shubham Sah <shubham.lal.sah@gmail.com>
2024-03-24 23:00:04 -0500

    f

--- V3/v3servotest.py
+++ V3/v3servotest.py
@@ -3,6 +3,6 @@ from adafruit_servokit import ServoKit
 
 kit = ServoKit(channels=16)
 
-kit.servo[1].angle = 120 
-kit.servo[2].angle = 120
+kit.servo[1].angle = 90 
+kit.servo[2].angle = 90
 time.sleep(1)
