31988fd015fe2a2e24a53a2d3f96edb3eb08ebe8
Shubham Sah <shubham.lal.sah@gmail.com>
2024-03-25 22:52:41 -0500

    f

--- V3/v3servotest.py
+++ V3/v3servotest.py
@@ -7,7 +7,7 @@ angle = 90
 
 # kit.servo[1].angle = angle 
 # kit.servo[2].angle = 10
-kit.servo[3].angle = 90
+kit.servo[3].angle = 110
 # kit.servo[4].angle = angle 
 # kit.servo[5].angle = angle 
 # kit.servo[6].angle = angle 
