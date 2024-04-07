59ec9a98069913c28b4a028855759e8ce20912d5
Shubham Sah <shubham.lal.sah@gmail.com>
2024-04-07 15:53:30 -0500

    f

--- V3/v3servotest.py
+++ V3/v3servotest.py
@@ -4,16 +4,47 @@ from adafruit_servokit import ServoKit
 kit = ServoKit(channels=16)
 
 angle = 90
+angle2 = 110
 
-kit.servo[1].angle = 110
-kit.servo[2].angle = 110
+kit.servo[1].angle = angle
+kit.servo[2].angle = angle
 kit.servo[3].angle = 85
-kit.servo[4].angle = 110
-kit.servo[5].angle = 110
+kit.servo[4].angle = angle
+kit.servo[5].angle = angle
 kit.servo[6].angle = 90
-kit.servo[7].angle = 110
-kit.servo[8].angle = 110
+kit.servo[7].angle = angle
+kit.servo[8].angle = angle
 kit.servo[9].angle = 95
-kit.servo[10].angle = 110
-kit.servo[11].angle = 110
-kit.servo[12].angle = angle 
+kit.servo[10].angle = angle
+kit.servo[11].angle = angle
+kit.servo[12].angle = 90
+
+time.sleep(2)
+
+kit.servo[1].angle = angle2
+kit.servo[2].angle = angle2
+kit.servo[3].angle = 85
+kit.servo[4].angle = angle2
+kit.servo[5].angle = angle2
+kit.servo[6].angle = 90
+kit.servo[7].angle = angle2
+kit.servo[8].angle = angle2
+kit.servo[9].angle = 95
+kit.servo[10].angle = angle2
+kit.servo[11].angle = angle2
+kit.servo[12].angle = 90
+
+time.sleep(2)
+
+kit.servo[1].angle = angle
+kit.servo[2].angle = angle
+kit.servo[3].angle = 85
+kit.servo[4].angle = angle
+kit.servo[5].angle = angle
+kit.servo[6].angle = 90
+kit.servo[7].angle = angle
+kit.servo[8].angle = angle
+kit.servo[9].angle = 95
+kit.servo[10].angle = angle
+kit.servo[11].angle = angle
+kit.servo[12].angle = 90
