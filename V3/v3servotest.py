d7b3dc67aa7efc122826fbf61169f20456b61db3
Shubham Sah <shubham.lal.sah@gmail.com>
2024-04-07 15:54:22 -0500

    test

--- V3/v3servotest.py
+++ V3/v3servotest.py
@@ -19,7 +19,7 @@ kit.servo[10].angle = angle
 kit.servo[11].angle = angle
 kit.servo[12].angle = 90
 
-time.sleep(2)
+time.sleep(1)
 
 kit.servo[1].angle = angle2
 kit.servo[2].angle = angle2
@@ -34,7 +34,67 @@ kit.servo[10].angle = angle2
 kit.servo[11].angle = angle2
 kit.servo[12].angle = 90
 
-time.sleep(2)
+time.sleep(1)
+
+kit.servo[1].angle = angle
+kit.servo[2].angle = angle
+kit.servo[3].angle = 85
+kit.servo[4].angle = angle
+kit.servo[5].angle = angle
+kit.servo[6].angle = 90
+kit.servo[7].angle = angle
+kit.servo[8].angle = angle
+kit.servo[9].angle = 95
+kit.servo[10].angle = angle
+kit.servo[11].angle = angle
+kit.servo[12].angle = 90
+
+time.sleep(1)
+
+kit.servo[1].angle = angle2
+kit.servo[2].angle = angle2
+kit.servo[3].angle = 85
+kit.servo[4].angle = angle2
+kit.servo[5].angle = angle2
+kit.servo[6].angle = 90
+kit.servo[7].angle = angle2
+kit.servo[8].angle = angle2
+kit.servo[9].angle = 95
+kit.servo[10].angle = angle2
+kit.servo[11].angle = angle2
+kit.servo[12].angle = 90
+
+time.sleep(1)
+
+kit.servo[1].angle = angle
+kit.servo[2].angle = angle
+kit.servo[3].angle = 85
+kit.servo[4].angle = angle
+kit.servo[5].angle = angle
+kit.servo[6].angle = 90
+kit.servo[7].angle = angle
+kit.servo[8].angle = angle
+kit.servo[9].angle = 95
+kit.servo[10].angle = angle
+kit.servo[11].angle = angle
+kit.servo[12].angle = 90
+
+time.sleep(1)
+
+kit.servo[1].angle = angle2
+kit.servo[2].angle = angle2
+kit.servo[3].angle = 85
+kit.servo[4].angle = angle2
+kit.servo[5].angle = angle2
+kit.servo[6].angle = 90
+kit.servo[7].angle = angle2
+kit.servo[8].angle = angle2
+kit.servo[9].angle = 95
+kit.servo[10].angle = angle2
+kit.servo[11].angle = angle2
+kit.servo[12].angle = 90
+
+time.sleep(1)
 
 kit.servo[1].angle = angle
 kit.servo[2].angle = angle
