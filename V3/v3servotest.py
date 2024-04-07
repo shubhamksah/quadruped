bc5ea25db8c20eab56db66414cdf3677f18339ff
Shubham Sah <shubham.lal.sah@gmail.com>
2024-04-07 16:02:40 -0500

    test

--- V3/v3servotest.py
+++ V3/v3servotest.py
@@ -4,8 +4,8 @@ from adafruit_servokit import ServoKit
 kit = ServoKit(channels=16)
 
 angle = 90
-angle2 = 50
-angle3 = 130
+angle2 = 40
+angle3 = 140
 
 kit.servo[1].angle = angle
 # kit.servo[2].angle = angle
@@ -35,77 +35,77 @@ kit.servo[10].angle = angle3
 # kit.servo[11].angle = angle3
 kit.servo[12].angle = 90
 
-time.sleep(1)
+# time.sleep(1)
 
-kit.servo[1].angle = angle
-# kit.servo[2].angle = angle
-kit.servo[3].angle = 85
-kit.servo[4].angle = angle
-# kit.servo[5].angle = angle
-kit.servo[6].angle = 90
-kit.servo[7].angle = angle
-# kit.servo[8].angle = angle
-kit.servo[9].angle = 95
-kit.servo[10].angle = angle
-# kit.servo[11].angle = angle
-kit.servo[12].angle = 90
+# kit.servo[1].angle = angle
+# # kit.servo[2].angle = angle
+# kit.servo[3].angle = 85
+# kit.servo[4].angle = angle
+# # kit.servo[5].angle = angle
+# kit.servo[6].angle = 90
+# kit.servo[7].angle = angle
+# # kit.servo[8].angle = angle
+# kit.servo[9].angle = 95
+# kit.servo[10].angle = angle
+# # kit.servo[11].angle = angle
+# kit.servo[12].angle = 90
 
-time.sleep(1)
+# time.sleep(1)
 
-kit.servo[1].angle = angle3
-# kit.servo[2].angle = angle3
-kit.servo[3].angle = 85
-kit.servo[4].angle = angle2
-# kit.servo[5].angle = angle2
-kit.servo[6].angle = 90
-kit.servo[7].angle = angle2
-# kit.servo[8].angle = angle2
-kit.servo[9].angle = 95
-kit.servo[10].angle = angle3
-# kit.servo[11].angle = angle3
-kit.servo[12].angle = 90
+# kit.servo[1].angle = angle3
+# # kit.servo[2].angle = angle3
+# kit.servo[3].angle = 85
+# kit.servo[4].angle = angle2
+# # kit.servo[5].angle = angle2
+# kit.servo[6].angle = 90
+# kit.servo[7].angle = angle2
+# # kit.servo[8].angle = angle2
+# kit.servo[9].angle = 95
+# kit.servo[10].angle = angle3
+# # kit.servo[11].angle = angle3
+# kit.servo[12].angle = 90
 
-time.sleep(1)
+# time.sleep(1)
 
-kit.servo[1].angle = angle
-# kit.servo[2].angle = angle
-kit.servo[3].angle = 85
-kit.servo[4].angle = angle
-# kit.servo[5].angle = angle
-kit.servo[6].angle = 90
-kit.servo[7].angle = angle
-# kit.servo[8].angle = angle
-kit.servo[9].angle = 95
-kit.servo[10].angle = angle
-# kit.servo[11].angle = angle
-kit.servo[12].angle = 90
+# kit.servo[1].angle = angle
+# # kit.servo[2].angle = angle
+# kit.servo[3].angle = 85
+# kit.servo[4].angle = angle
+# # kit.servo[5].angle = angle
+# kit.servo[6].angle = 90
+# kit.servo[7].angle = angle
+# # kit.servo[8].angle = angle
+# kit.servo[9].angle = 95
+# kit.servo[10].angle = angle
+# # kit.servo[11].angle = angle
+# kit.servo[12].angle = 90
 
-time.sleep(1)
+# time.sleep(1)
 
-kit.servo[1].angle = angle3
-# kit.servo[2].angle = angle3
-kit.servo[3].angle = 85
-kit.servo[4].angle = angle2
-# kit.servo[5].angle = angle2
-kit.servo[6].angle = 90
-kit.servo[7].angle = angle2
-# kit.servo[8].angle = angle2
-kit.servo[9].angle = 95
-kit.servo[10].angle = angle3
-# kit.servo[11].angle = angle3
-kit.servo[12].angle = 90
+# kit.servo[1].angle = angle3
+# # kit.servo[2].angle = angle3
+# kit.servo[3].angle = 85
+# kit.servo[4].angle = angle2
+# # kit.servo[5].angle = angle2
+# kit.servo[6].angle = 90
+# kit.servo[7].angle = angle2
+# # kit.servo[8].angle = angle2
+# kit.servo[9].angle = 95
+# kit.servo[10].angle = angle3
+# # kit.servo[11].angle = angle3
+# kit.servo[12].angle = 90
 
-time.sleep(1)
+# time.sleep(1)
 
-kit.servo[1].angle = angle
-# kit.servo[2].angle = angle
-kit.servo[3].angle = 85
-kit.servo[4].angle = angle
-# kit.servo[5].angle = angle
-kit.servo[6].angle = 90
-kit.servo[7].angle = angle
-# kit.servo[8].angle = angle
-kit.servo[9].angle = 95
-kit.servo[10].angle = angle
-# kit.servo[11].angle = angle
-kit.servo[12].angle = 90
+# kit.servo[1].angle = angle
+# # kit.servo[2].angle = angle
+# kit.servo[3].angle = 85
+# kit.servo[4].angle = angle
+# # kit.servo[5].angle = angle
+# kit.servo[6].angle = 90
+# kit.servo[7].angle = angle
+# # kit.servo[8].angle = angle
+# kit.servo[9].angle = 95
+# kit.servo[10].angle = angle
+# # kit.servo[11].angle = angle
+# kit.servo[12].angle = 90
