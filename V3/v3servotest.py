a21b275dcba1116a088687193dcb215a020110f2
Shubham Sah <shubham.lal.sah@gmail.com>
2024-04-07 15:59:11 -0500

    f

--- V3/v3servotest.py
+++ V3/v3servotest.py
@@ -4,8 +4,8 @@ from adafruit_servokit import ServoKit
 kit = ServoKit(channels=16)
 
 angle = 90
-angle2 = 130
-angle3 = 50
+angle2 = 70
+angle3 = 110
 
 kit.servo[1].angle = angle
 kit.servo[2].angle = angle
