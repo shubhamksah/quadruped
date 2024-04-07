b9c867efcfb92db585ad53b839ef0c3baf4905c3
Shubham Sah <shubham.lal.sah@gmail.com>
2024-04-07 15:10:27 -0500

    f

--- V3/v3servotest.py
+++ V3/v3servotest.py
@@ -5,12 +5,12 @@ kit = ServoKit(channels=16)
 
 angle = 90
 
-kit.servo[1].angle = 0
-kit.servo[2].angle = 0
-kit.servo[3].angle = 84
-# kit.servo[4].angle = angle 
-# kit.servo[5].angle = angle 
-# kit.servo[6].angle = angle 
+# kit.servo[1].angle = 0
+# kit.servo[2].angle = 0
+# kit.servo[3].angle = 84
+kit.servo[4].angle = 90
+kit.servo[5].angle = 90
+kit.servo[6].angle = 90 
 # kit.servo[7].angle = angle 
 # kit.servo[8].angle = angle 
 # kit.servo[9].angle = angle 
