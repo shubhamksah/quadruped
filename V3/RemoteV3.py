17e017efe3e1f86c75c63e7deeb806fd4a7eee6d
Shubham Sah <shubham.lal.sah@gmail.com>
2024-04-14 16:58:37 -0500

    f

--- V3/RemoteV3.py
+++ V3/RemoteV3.py
@@ -10,15 +10,15 @@ def standing_position(FL_TIBIA,FL_FEMUR,FL_HIP,FR_TIBIA,FR_FEMUR,FR_HIP,BR_TIBIA
     kit.servo[1].angle = angle
     kit.servo[2].angle = angle
     kit.servo[3].angle = 85
-    kit.servo[4].angle = angle
-    kit.servo[5].angle = angle
-    kit.servo[6].angle = 90
-    kit.servo[7].angle = angle
-    kit.servo[8].angle = angle
-    kit.servo[9].angle = 95
-    kit.servo[10].angle = angle
-    kit.servo[11].angle = angle
-    kit.servo[12].angle = 90        
+    # kit.servo[4].angle = angle
+    # kit.servo[5].angle = angle
+    # kit.servo[6].angle = 90
+    # kit.servo[7].angle = angle
+    # kit.servo[8].angle = angle
+    # kit.servo[9].angle = 95
+    # kit.servo[10].angle = angle
+    # kit.servo[11].angle = angle
+    # kit.servo[12].angle = 90        
     
 
 
