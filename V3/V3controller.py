2b1332c7cdd852e008316b975bfd59fd50950c63
Shubham Sah <shubham.lal.sah@gmail.com>
2024-08-02 17:58:30 -0500

    f

--- V3/V3controller.py
+++ V3/V3controller.py
@@ -4,6 +4,19 @@ from adafruit_servokit import ServoKit
 
 kit = ServoKit(channels=16)
 
+FLT_angle = 90
+FLF_angle = 90
+FLH_angle = 90
+FRT_angle = 90
+FRF_angle = 90
+FRH_angle = 90
+BLT_angle = 90
+BLF_angle = 90
+BLH_angle = 90
+BRT_angle = 90
+BRF_angle = 90
+BRH_angle = 90
+
 def stand(FL1,FL2,FL3,FR1,FR2,FR3,BL1,BL2,BL3,BR1,BR2,BR3):
 
     kit.servo[FL1].angle = 90
@@ -19,20 +32,48 @@ def stand(FL1,FL2,FL3,FR1,FR2,FR3,BL1,BL2,BL3,BR1,BR2,BR3):
     kit.servo[BR2].angle = 90
     kit.servo[BR3].angle = 90  
 
+    FLT_angle = 90
+    FLF_angle = 90
+    FLH_angle = 90
+    FRT_angle = 90
+    FRF_angle = 90
+    FRH_angle = 90
+    BLT_angle = 90
+    BLF_angle = 90
+    BLH_angle = 90
+    BRT_angle = 90
+    BRF_angle = 90
+    BRH_angle = 90
+
+
 def rest(FL1,FL2,FL3,FR1,FR2,FR3,BL1,BL2,BL3,BR1,BR2,BR3):
 
-    kit.servo[FL1].angle = 90
-    kit.servo[FL2].angle = 125
-    kit.servo[FL3].angle = 85
-    kit.servo[FR1].angle = 90
-    kit.servo[FR2].angle = 50
+    kit.servo[FL1].angle = 50
+    kit.servo[FL2].angle = 150
+    kit.servo[FL3].angle = 90
+    kit.servo[FR1].angle = 50
+    kit.servo[FR2].angle = 150
     kit.servo[FR3].angle = 90
-    kit.servo[BL1].angle = 90
-    kit.servo[BL2].angle = 135
-    kit.servo[BL3].angle = 95
-    kit.servo[BR1].angle = 90
-    kit.servo[BR2].angle = 40
-    kit.servo[BR3].angle = 95  
+    kit.servo[BL1].angle = 130
+    kit.servo[BL2].angle = 30
+    kit.servo[BL3].angle = 90
+    kit.servo[BR1].angle = 130
+    kit.servo[BR2].angle = 30
+    kit.servo[BR3].angle = 90 
+
+    FLT_angle = 50
+    FLF_angle = 150
+    FLH_angle = 90
+    FRT_angle = 50
+    FRF_angle = 150
+    FRH_angle = 90
+    BLT_angle = 130
+    BLF_angle = 30
+    BLH_angle = 90
+    BRT_angle = 130
+    BRF_angle = 30
+    BRH_angle = 90
+
 
     
 t = 0
@@ -52,6 +93,10 @@ try:
             axis = [joy1.get_axis(0),joy1.get_axis(1),joy1.get_axis(2),joy1.get_axis(3),joy1.get_axis(4),joy1.get_axis(5)]
 
             if joy1.get_axis(1) < -0.5:
+
+                tpos = 90
+                tvar = 5
+                tmove = tpos + tvar
 
                 time.sleep(0.05)
 
@@ -78,10 +123,12 @@ try:
             if joy1.get_button(1) > 0.5:
 
                 rest(1,2,3,4,5,6,7,8,9,10,11,12)
+                time.sleep(0.05)
             
             if joy1.get_button(3) > 0.5:
                 
                 stand(1,2,3,4,5,6,7,8,9,10,11,12)
+                time.sleep(0.05)
 
             t = joy1.get_button(0)
 
