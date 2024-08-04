61b621c8883c59b5036675cc0d34007e02401ecb
Shubham Sah <shubham.lal.sah@gmail.com>
2024-08-03 20:59:15 -0500

    f

--- V3/V3defaultpos.py
+++ V3/V3defaultpos.py
@@ -8,7 +8,7 @@ try:
     kit.servo[2].angle = 90
     kit.servo[3].angle = 110
     kit.servo[4].angle = 90
-    kit.servo[5].angle = 85
+    kit.servo[5].angle = 80
     kit.servo[6].angle = 90
     kit.servo[7].angle = 90
     kit.servo[8].angle = 90
