b874f6cf85a5d68a5d46b9b9e839ff0c5b5700e9
Shubham Sah <shubham.lal.sah@gmail.com>
2024-08-04 12:40:19 -0500

    f

--- V3/V3controller.py
+++ V3/V3controller.py
@@ -2,8 +2,7 @@ import time
 import pygame
 from adafruit_servokit import ServoKit
 
-kit = ServoKit(channels=16)
-
+# Initializing servo angle variables
 FLT_angle = 90
 FLF_angle = 90
 FLH_angle = 90
@@ -17,7 +16,89 @@ BRT_angle = 90
 BRF_angle = 90
 BRH_angle = 90
 
-def stand(FL1,FL2,FL3,FR1,FR2,FR3,BL1,BL2,BL3,BR1,BR2,BR3):
+def main():
+    print("Running Quadruped")
+
+    kit = ServoKit(channels=16)
+        
+    t = 0
+
+    try:
+
+        stand()
+
+        pygame.init()
+        joy1 = pygame.joystick.Joystick(0)
+        joy1.init()
+
+        while t == 0:
+            try:
+                pygame.event.pump()
+                buttons = [joy1.get_button(0),joy1.get_button(1),joy1.get_button(2),joy1.get_button(3),joy1.get_button(4),joy1.get_button(5)]
+                axis = [joy1.get_axis(0),joy1.get_axis(1),joy1.get_axis(2),joy1.get_axis(3),joy1.get_axis(4),joy1.get_axis(5)]
+
+                if joy1.get_axis(1) < -0.5:
+
+                    FLT_move = FLT_angle + 5
+                    FRT_move = FRT_angle + 5
+                    BLT_move = BLT_angle - 5
+                    BRT_move = BRT_angle - 5
+                    
+                    FLT_angle = FLT_move
+                    FRT_angle = FRT_move
+                    BLT_angle = BLT_move
+                    BRT_angle = BRT_move
+
+                    kit.servo[0].angle = FLT_move
+                    kit.servo[3].angle = FRT_move
+                    kit.servo[6].angle = BLT_move
+                    kit.servo[9].angle = BRT_move
+
+                    time.sleep(0.05)
+
+                if joy1.get_axis(1) > 0.5:
+
+                    time.sleep(0.05)
+
+                if joy1.get_axis(2) < -0.5:
+
+                    time.sleep(0.05)
+
+                if joy1.get_axis(2) > 0.5:
+
+                    time.sleep(0.05)
+
+                if joy1.get_axis(3) > 0.5:
+                
+                    time.sleep(0.05)
+
+                if joy1.get_axis(3) < -0.5:
+        
+                    time.sleep(0.05)
+
+                if joy1.get_button(1) > 0.5:
+
+                    rest()
+                    time.sleep(0.05)
+                
+                if joy1.get_button(3) > 0.5:
+                    
+                    stand()
+                    time.sleep(0.05)
+
+                t = joy1.get_button(0)
+
+            finally:
+                if t == 1:
+                    print("STOP")
+                    rest()
+
+        
+    except:
+        print("Exit")
+        quit()
+
+def stand():
 
     kit.servo[0].angle = 90
     kit.servo[1].angle = 135
@@ -32,21 +113,34 @@ def stand(FL1,FL2,FL3,FR1,FR2,FR3,BL1,BL2,BL3,BR1,BR2,BR3):
     kit.servo[10].angle = 45
     kit.servo[11].angle = 95  
 
+    global FLT_angle
+    global FLF_angle
+    global FLH_angle
+    global FRT_angle
+    global FRF_angle
+    global FRH_angle
+    global BLT_angle
+    global BLF_angle
+    global BLH_angle
+    global BRT_angle 
+    global BRF_angle 
+    global BRH_angle  
+
     FLT_angle = 90
-    FLF_angle = 90
-    FLH_angle = 90
-    FRT_angle = 90
-    FRF_angle = 90
-    FRH_angle = 90
-    BLT_angle = 90
-    BLF_angle = 90
-    BLH_angle = 90
-    BRT_angle = 90
-    BRF_angle = 90
-    BRH_angle = 90
+    FLF_angle = 135
+    FLH_angle = 100
+    FRT_angle = 97
+    FRF_angle = 45
+    FRH_angle = 88
+    BLT_angle = 88
+    BLF_angle = 130
+    BLH_angle = 95
+    BRT_angle = 94
+    BRF_angle = 45
+    BRH_angle = 95
 
 
-def rest(FL1,FL2,FL3,FR1,FR2,FR3,BL1,BL2,BL3,BR1,BR2,BR3):
+def rest():
 
     kit.servo[0].angle = 50
     kit.servo[1].angle = 150
@@ -61,83 +155,31 @@ def rest(FL1,FL2,FL3,FR1,FR2,FR3,BL1,BL2,BL3,BR1,BR2,BR3):
     kit.servo[10].angle = 30
     kit.servo[11].angle = 95  
 
+    global FLT_angle
+    global FLF_angle
+    global FLH_angle
+    global FRT_angle
+    global FRF_angle
+    global FRH_angle
+    global BLT_angle
+    global BLF_angle
+    global BLH_angle
+    global BRT_angle 
+    global BRF_angle 
+    global BRH_angle     
+    
     FLT_angle = 50
     FLF_angle = 150
-    FLH_angle = 90
-    FRT_angle = 50
-    FRF_angle = 150
-    FRH_angle = 90
-    BLT_angle = 130
-    BLF_angle = 30
-    BLH_angle = 90
-    BRT_angle = 130
+    FLH_angle = 100
+    FRT_angle = 137
+    FRF_angle = 35
+    FRH_angle = 88
+    BLT_angle = 48
+    BLF_angle = 145
+    BLH_angle = 95
+    BRT_angle = 134
     BRF_angle = 30
-    BRH_angle = 90
+    BRH_angle = 95  
 
-
-    
-t = 0
-
-try:
-
-    stand(0,1,2,3,4,5,6,7,8,9,10,11,12)
-
-    pygame.init()
-    joy1 = pygame.joystick.Joystick(0)
-    joy1.init()
-
-    while t == 0:
-        try:
-            pygame.event.pump()
-            buttons = [joy1.get_button(0),joy1.get_button(1),joy1.get_button(2),joy1.get_button(3),joy1.get_button(4),joy1.get_button(5)]
-            axis = [joy1.get_axis(0),joy1.get_axis(1),joy1.get_axis(2),joy1.get_axis(3),joy1.get_axis(4),joy1.get_axis(5)]
-
-            if joy1.get_axis(1) < -0.5:
-
-                tpos = 90
-                tvar = 5
-                tmove = tpos + tvar
-
-                time.sleep(0.05)
-
-            if joy1.get_axis(1) > 0.5:
-
-                time.sleep(0.05)
-
-            if joy1.get_axis(2) < -0.5:
-
-                time.sleep(0.05)
-
-            if joy1.get_axis(2) > 0.5:
-
-                time.sleep(0.05)
-
-            if joy1.get_axis(3) > 0.5:
-             
-                time.sleep(0.05)
-
-            if joy1.get_axis(3) < -0.5:
-     
-                time.sleep(0.05)
-
-            if joy1.get_button(1) > 0.5:
-
-                rest(1,2,3,4,5,6,7,8,9,10,11,12)
-                time.sleep(0.05)
-            
-            if joy1.get_button(3) > 0.5:
-                
-                stand(1,2,3,4,5,6,7,8,9,10,11,12)
-                time.sleep(0.05)
-
-            t = joy1.get_button(0)
-
-        finally:
-            if t == 1:
-                print("STOP")
-                rest(0,1,2,3,4,5,6,7,8,9,10,11)
-
-    
-except:
-    print("Exit")
-    quit()
+if __name__ == "__main__":
+    main()
