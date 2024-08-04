02f7439eb900c3209a1804ce6d1a3ebcf59b99aa
Shubham Sah <shubham.lal.sah@gmail.com>
2024-08-04 11:15:59 -0500

    f

--- V3/V3controller.py
+++ V3/V3controller.py
@@ -19,18 +19,18 @@ BRH_angle = 90
 
 def stand(FL1,FL2,FL3,FR1,FR2,FR3,BL1,BL2,BL3,BR1,BR2,BR3):
 
-    kit.servo[FL1].angle = 90
-    kit.servo[FL2].angle = 90
-    kit.servo[FL3].angle = 90
-    kit.servo[FR1].angle = 90
-    kit.servo[FR2].angle = 90
-    kit.servo[FR3].angle = 90
-    kit.servo[BL1].angle = 90
-    kit.servo[BL2].angle = 90
-    kit.servo[BL3].angle = 90
-    kit.servo[BR1].angle = 90
-    kit.servo[BR2].angle = 90
-    kit.servo[BR3].angle = 90  
+    kit.servo[0].angle = 90
+    kit.servo[1].angle = 135
+    kit.servo[2].angle = 100
+    kit.servo[3].angle = 97
+    kit.servo[4].angle = 45
+    kit.servo[5].angle = 88
+    kit.servo[6].angle = 88
+    kit.servo[7].angle = 130
+    kit.servo[8].angle = 95
+    kit.servo[9].angle = 94
+    kit.servo[10].angle = 45
+    kit.servo[11].angle = 95  
 
     FLT_angle = 90
     FLF_angle = 90
@@ -48,18 +48,18 @@ def stand(FL1,FL2,FL3,FR1,FR2,FR3,BL1,BL2,BL3,BR1,BR2,BR3):
 
 def rest(FL1,FL2,FL3,FR1,FR2,FR3,BL1,BL2,BL3,BR1,BR2,BR3):
 
-    kit.servo[FL1].angle = 50
-    kit.servo[FL2].angle = 150
-    kit.servo[FL3].angle = 90
-    kit.servo[FR1].angle = 50
-    kit.servo[FR2].angle = 150
-    kit.servo[FR3].angle = 90
-    kit.servo[BL1].angle = 130
-    kit.servo[BL2].angle = 30
-    kit.servo[BL3].angle = 90
-    kit.servo[BR1].angle = 130
-    kit.servo[BR2].angle = 30
-    kit.servo[BR3].angle = 90 
+    kit.servo[0].angle = 50
+    kit.servo[1].angle = 150
+    kit.servo[2].angle = 100
+    kit.servo[3].angle = 137
+    kit.servo[4].angle = 35
+    kit.servo[5].angle = 88
+    kit.servo[6].angle = 48
+    kit.servo[7].angle = 145
+    kit.servo[8].angle = 95
+    kit.servo[9].angle = 134
+    kit.servo[10].angle = 30
+    kit.servo[11].angle = 95  
 
     FLT_angle = 50
     FLF_angle = 150
@@ -80,7 +80,7 @@ t = 0
 
 try:
 
-    stand(1,2,3,4,5,6,7,8,9,10,11,12)
+    stand(0,1,2,3,4,5,6,7,8,9,10,11,12)
 
     pygame.init()
     joy1 = pygame.joystick.Joystick(0)
@@ -135,7 +135,7 @@ try:
         finally:
             if t == 1:
                 print("STOP")
-                rest(1,2,3,4,5,6,7,8,9,10,11,12)
+                rest(0,1,2,3,4,5,6,7,8,9,10,11)
 
     
 except:
