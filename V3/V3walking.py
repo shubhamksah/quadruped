95699bee174074a14163df4c0870d961cf574760
Shubham Sah <shubham.lal.sah@gmail.com>
2024-08-04 18:09:37 -0500

    f

--- V3/V3walking.py
+++ V3/V3walking.py
@@ -62,18 +62,18 @@ def main():
             # print("Position 2")
             # time.sleep(0.2)
 
-            # kit.servo[0].angle = FLT - 7 #Down
-            # kit.servo[1].angle = FLF - 3 #Down
-            # kit.servo[2].angle = FLH
-            # kit.servo[3].angle = FRT - 26 #Up
-            # kit.servo[4].angle = FRF - 27 #Up
-            # kit.servo[5].angle = FRH
-            # kit.servo[6].angle = BLT + 26 #Up
-            # kit.servo[7].angle = BLF + 27 #Up
-            # kit.servo[8].angle = BLH
-            # kit.servo[9].angle = BRT + 7 #Down
-            # kit.servo[10].angle = BRF + 3 #Down
-            # kit.servo[11].angle = BRH 
+            kit.servo[0].angle = FLT - 7 #Down
+            kit.servo[1].angle = FLF - 3 #Down
+            kit.servo[2].angle = FLH
+            kit.servo[3].angle = FRT - 26 #Up
+            kit.servo[4].angle = FRF - 27 #Up
+            kit.servo[5].angle = FRH
+            kit.servo[6].angle = BLT + 26 #Up
+            kit.servo[7].angle = BLF + 27 #Up
+            kit.servo[8].angle = BLH
+            kit.servo[9].angle = BRT + 7 #Down
+            kit.servo[10].angle = BRF + 3 #Down
+            kit.servo[11].angle = BRH 
 
             print("Position 3")
             time.sleep(1)
