95b624fe782cfc7d3ced83babf700e448e454478
Shubham Sah <shubham.lal.sah@gmail.com>
2024-08-03 20:33:23 -0500

    f

--- V3/V3controller.py
+++ V3/V3controller.py
@@ -21,7 +21,7 @@ def stand(FL1,FL2,FL3,FR1,FR2,FR3,BL1,BL2,BL3,BR1,BR2,BR3):
 
     kit.servo[FL1].angle = 90
     kit.servo[FL2].angle = 90
-    kit.servo[FL3].angle = 90
+    kit.servo[FL3].angle = 100
     kit.servo[FR1].angle = 90
     kit.servo[FR2].angle = 90
     kit.servo[FR3].angle = 90
@@ -82,60 +82,60 @@ try:
 
     stand(1,2,3,4,5,6,7,8,9,10,11,12)
 
-    pygame.init()
-    joy1 = pygame.joystick.Joystick(0)
-    joy1.init()
+    # pygame.init()
+    # joy1 = pygame.joystick.Joystick(0)
+    # joy1.init()
 
-    while t == 0:
-        try:
-            pygame.event.pump()
-            buttons = [joy1.get_button(0),joy1.get_button(1),joy1.get_button(2),joy1.get_button(3),joy1.get_button(4),joy1.get_button(5)]
-            axis = [joy1.get_axis(0),joy1.get_axis(1),joy1.get_axis(2),joy1.get_axis(3),joy1.get_axis(4),joy1.get_axis(5)]
+    # while t == 0:
+    #     try:
+    #         pygame.event.pump()
+    #         buttons = [joy1.get_button(0),joy1.get_button(1),joy1.get_button(2),joy1.get_button(3),joy1.get_button(4),joy1.get_button(5)]
+    #         axis = [joy1.get_axis(0),joy1.get_axis(1),joy1.get_axis(2),joy1.get_axis(3),joy1.get_axis(4),joy1.get_axis(5)]
 
-            if joy1.get_axis(1) < -0.5:
+    #         if joy1.get_axis(1) < -0.5:
 
-                tpos = 90
-                tvar = 5
-                tmove = tpos + tvar
+    #             tpos = 90
+    #             tvar = 5
+    #             tmove = tpos + tvar
 
-                time.sleep(0.05)
+    #             time.sleep(0.05)
 
-            if joy1.get_axis(1) > 0.5:
+    #         if joy1.get_axis(1) > 0.5:
 
-                time.sleep(0.05)
+    #             time.sleep(0.05)
 
-            if joy1.get_axis(2) < -0.5:
+    #         if joy1.get_axis(2) < -0.5:
 
-                time.sleep(0.05)
+    #             time.sleep(0.05)
 
-            if joy1.get_axis(2) > 0.5:
+    #         if joy1.get_axis(2) > 0.5:
 
-                time.sleep(0.05)
+    #             time.sleep(0.05)
 
-            if joy1.get_axis(3) > 0.5:
+    #         if joy1.get_axis(3) > 0.5:
              
-                time.sleep(0.05)
+    #             time.sleep(0.05)
 
-            if joy1.get_axis(3) < -0.5:
+    #         if joy1.get_axis(3) < -0.5:
      
-                time.sleep(0.05)
+    #             time.sleep(0.05)
 
-            if joy1.get_button(1) > 0.5:
+    #         if joy1.get_button(1) > 0.5:
 
-                rest(1,2,3,4,5,6,7,8,9,10,11,12)
-                time.sleep(0.05)
+    #             rest(1,2,3,4,5,6,7,8,9,10,11,12)
+    #             time.sleep(0.05)
             
-            if joy1.get_button(3) > 0.5:
+    #         if joy1.get_button(3) > 0.5:
                 
-                stand(1,2,3,4,5,6,7,8,9,10,11,12)
-                time.sleep(0.05)
+    #             stand(1,2,3,4,5,6,7,8,9,10,11,12)
+    #             time.sleep(0.05)
 
-            t = joy1.get_button(0)
+    #         t = joy1.get_button(0)
 
-        finally:
-            if t == 1:
-                print("STOP")
-                rest(1,2,3,4,5,6,7,8,9,10,11,12)
+    #     finally:
+    #         if t == 1:
+    #             print("STOP")
+    #             rest(1,2,3,4,5,6,7,8,9,10,11,12)
 
     
 except:
