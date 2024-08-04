5c0e2df3d9fc972deb1b5aa2fbc65651da601b48
Shubham Sah <shubham.lal.sah@gmail.com>
2024-08-04 11:14:31 -0500

    f

--- V3/V3defaultpos.py
+++ V3/V3defaultpos.py
@@ -5,16 +5,16 @@ kit = ServoKit(channels=16)
 try:
 
     kit.servo[0].angle = 50
-    kit.servo[1].angle = 135
+    kit.servo[1].angle = 150
     kit.servo[2].angle = 100
     kit.servo[3].angle = 137
-    kit.servo[4].angle = 45
+    kit.servo[4].angle = 30
     kit.servo[5].angle = 88
     kit.servo[6].angle = 48
-    kit.servo[7].angle = 130
+    kit.servo[7].angle = 145
     kit.servo[8].angle = 95
     kit.servo[9].angle = 134
-    kit.servo[10].angle = 45
+    kit.servo[10].angle = 30
     kit.servo[11].angle = 95  
 
 except:
