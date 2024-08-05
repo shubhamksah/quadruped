14ba89749fd4e44dd01228ec2b9445ff31886c3c
Shubham Sah <shubham.lal.sah@gmail.com>
2024-08-04 19:33:39 -0500

    f

--- V3/V3defaultpos.py
+++ V3/V3defaultpos.py
@@ -19,16 +19,16 @@ BRH = 95
 try:
 
     kit.servo[0].angle = 90
-    kit.servo[1].angle = 135
+    kit.servo[1].angle = 137
     kit.servo[2].angle = 100
-    kit.servo[3].angle = 97
-    kit.servo[4].angle = 45
+    kit.servo[3].angle = 84
+    kit.servo[4].angle = 42
     kit.servo[5].angle = 88
     kit.servo[6].angle = 100
-    kit.servo[7].angle = 130
+    kit.servo[7].angle = 131
     kit.servo[8].angle = 95
-    kit.servo[9].angle = 94
-    kit.servo[10].angle = 45
+    kit.servo[9].angle = 93
+    kit.servo[10].angle = 41
     kit.servo[11].angle = 95   
 
 except:
