707c52b84aea8c546021feca9776fa2e916e1f02
Shubham Sah <shubham.lal.sah@gmail.com>
2024-08-07 13:13:33 -0500

    f

--- V3/V3testwalking.py
+++ V3/V3testwalking.py
@@ -23,7 +23,7 @@ def main():
         try:
 
             kit.servo[0].angle = 90
-            kit.servo[1].angle = 85
+            kit.servo[1].angle = 83
             kit.servo[2].angle = 100
 
             # kit.servo[2].angle = FLH
