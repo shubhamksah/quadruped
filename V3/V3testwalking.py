04ebedfd7b9a2af0cfa6eccd65dc74fd0c0a0ef2
Shubham Sah <shubham.lal.sah@gmail.com>
2024-08-07 15:25:22 -0500

    f

--- V3/V3testwalking.py
+++ V3/V3testwalking.py
@@ -70,7 +70,7 @@ def main():
             #FR Movement
 
             kit.servo[3].angle = 82 + 20
-            kit.servo[4].angle = 45
+            kit.servo[4].angle = 45 + 13
             kit.servo[5].angle = 88
 
             # time.sleep(0.1)
