2f252efab6c8179bfbfe31e51d9745a7aed62084
Shubham Sah <shubham.lal.sah@gmail.com>
2024-08-11 10:44:32 -0500

    f

--- V3/V3walking.py
+++ V3/V3walking.py
@@ -70,7 +70,7 @@ def main():
     kit.servo[4].angle = FRF
     kit.servo[5].angle = FRH
     kit.servo[6].angle = BLT
-    kit.servo[7].angle = BLF + 10
+    kit.servo[7].angle = BLF - 10
     kit.servo[8].angle = BLH
     kit.servo[9].angle = BRT
     kit.servo[10].angle = BRF
