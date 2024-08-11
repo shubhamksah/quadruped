901f4716fdd7831ac1c537e7d91511dff49bc08d
Shubham Sah <shubham.lal.sah@gmail.com>
2024-08-11 03:47:03 -0500

    f

--- V3/V3walking.py
+++ V3/V3walking.py
@@ -76,54 +76,56 @@ def main():
     kit.servo[10].angle = BRF
     kit.servo[11].angle = BRH  
 
+    #HIPS Lock
+    kit.servo[2].angle = FLH
+    kit.servo[5].angle = FRH
+    kit.servo[8].angle = BLH
+    kit.servo[11].angle = BRH  
+
+    #FL & BR UP
+    kit.servo[0].angle = FLT - Pos3T
+    kit.servo[9].angle = BRT + Pos3T
+
+    #BL & FR DOWN
+    kit.servo[3].angle = FRT + Pos1T
+    kit.servo[4].angle = FRF + Pos1F
+    kit.servo[6].angle = BLT - Pos1T
+    kit.servo[7].angle = BLF - Pos1F
+
+
+
+    time.sleep(MovementDelay)
+
+    #HIPS Lock
+    kit.servo[2].angle = FLH
+    kit.servo[5].angle = FRH
+    kit.servo[8].angle = BLH
+    kit.servo[11].angle = BRH  
+
+    #FL & BR UP
+    kit.servo[0].angle = FLT - Pos3T
+    kit.servo[9].angle = BRT + Pos3T
+
+    #FR & BR BACK
+    pos1move = Pos2FIncrement
+    pos2move = Pos2TIncrement
+    while pos2move <= Pos2T:
+        kit.servo[4].angle = FRF1 - pos1move
+        kit.servo[7].angle = BLF1 + pos1move
+        pos1move += Pos2FIncrement
+        kit.servo[3].angle = FRT1 - pos2move
+        kit.servo[6].angle = BLT1 + pos2move
+        pos2move += Pos2TIncrement
+        kit.servo[5].angle = FRH
+        time.sleep(Pos2delay)
+
+    time.sleep(MovementDelay)
+
     while t <= 0.05: #Movement Loop
         
         try:
             
-            #HIPS Lock
-            kit.servo[2].angle = FLH
-            kit.servo[5].angle = FRH
-            kit.servo[8].angle = BLH
-            kit.servo[11].angle = BRH  
 
-            #FL & BR UP
-            kit.servo[0].angle = FLT - Pos3T
-            kit.servo[9].angle = BRT + Pos3T
-
-            #BL & FR DOWN
-            kit.servo[3].angle = FRT + Pos1T
-            kit.servo[4].angle = FRF + Pos1F
-            kit.servo[6].angle = BLT - Pos1T
-            kit.servo[7].angle = BLF - Pos1F
-
-
-
-            time.sleep(MovementDelay)
-
-            #HIPS Lock
-            kit.servo[2].angle = FLH
-            kit.servo[5].angle = FRH
-            kit.servo[8].angle = BLH
-            kit.servo[11].angle = BRH  
-
-            #FL & BR UP
-            kit.servo[0].angle = FLT - Pos3T
-            kit.servo[9].angle = BRT + Pos3T
-
-            #FR & BR BACK
-            pos1move = Pos2FIncrement
-            pos2move = Pos2TIncrement
-            while pos2move <= Pos2T:
-                kit.servo[4].angle = FRF1 - pos1move
-                kit.servo[7].angle = BLF1 + pos1move
-                pos1move += Pos2FIncrement
-                kit.servo[3].angle = FRT1 - pos2move
-                kit.servo[6].angle = BLT1 + pos2move
-                pos2move += Pos2TIncrement
-                kit.servo[5].angle = FRH
-                time.sleep(Pos2delay)
-
-            time.sleep(MovementDelay)
 
 
 
