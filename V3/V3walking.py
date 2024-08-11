41fbc0607398fd8e2bf4a1528f78bf9369ca333a
Shubham Sah <shubham.lal.sah@gmail.com>
2024-08-11 15:40:57 -0500

    f

--- V3/V3walking.py
+++ V3/V3walking.py
@@ -48,7 +48,7 @@ Pos2FIncrement = Pos2F/160 #Femur Movement Increment Position 2
 
 Pos2delay = 0.000000001 #Position 2 Speed (Lower = Faster)
 
-MovementDelay = 0.1 #Full Movement Speed (Lower = Faster)
+MovementDelay = 0.01 #Full Movement Speed (Lower = Faster)
 
 
 #Position Down = Tibia (20 Down), Femur (13 Forward)
