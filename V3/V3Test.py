30191c4133a49e95321783df278a75cf13656682
Shubham Sah <shubham.lal.sah@gmail.com>
2024-08-17 17:50:18 -0500

    f

--- V3/V3Test.py
+++ V3/V3Test.py
@@ -66,8 +66,8 @@ def main():
     kit.servo[0].angle = FLT
     kit.servo[1].angle = FLF
     kit.servo[2].angle = FLH
-    kit.servo[3].angle = 95
-    kit.servo[4].angle = 90
+    kit.servo[3].angle = 90
+    kit.servo[4].angle = 95
     kit.servo[5].angle = FRH
     kit.servo[6].angle = 90
     kit.servo[7].angle = 90
