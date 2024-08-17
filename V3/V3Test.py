c04c72c039fbee99588d2f4c4e47ad8a1793945e
Shubham Sah <shubham.lal.sah@gmail.com>
2024-08-17 18:10:06 -0500

    f

--- V3/V3Test.py
+++ V3/V3Test.py
@@ -68,12 +68,12 @@ def main():
     kit.servo[2].angle = FLH
     kit.servo[3].angle = FRT
     kit.servo[4].angle = FRF
-    kit.servo[5].angle = FRH
+    kit.servo[5].angle = FRH + 1
     kit.servo[6].angle = BLT
     kit.servo[7].angle = BLF - 4
     kit.servo[8].angle = BLH
     kit.servo[9].angle = BRT
-    kit.servo[10].angle = BRF + 1
+    kit.servo[10].angle = BRF + 2
     kit.servo[11].angle = BRH  
 
     # while t <= 3.05:
