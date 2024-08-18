1efd6da78cf7e6e81c3a9f7611c3ba496b9e3515
Shubham Sah <shubham.lal.sah@gmail.com>
2024-08-17 20:31:19 -0500

    f

--- V3/V3Test.py
+++ V3/V3Test.py
@@ -86,17 +86,17 @@ def main():
     kit.servo[8].angle = BLH
     kit.servo[11].angle = BRH 
 
-    kit.servo[0].angle = FLT + 12 + 35
-    kit.servo[1].angle = FLF + 9 + 12
+    kit.servo[0].angle = FLT + 12 + 40
+    kit.servo[1].angle = FLF + 9 + 15
 
-    kit.servo[3].angle = FRT - 10 - 35
-    kit.servo[4].angle = FRF - 9 - 12
+    kit.servo[3].angle = FRT - 10 - 40
+    kit.servo[4].angle = FRF - 9 - 15
 
-    kit.servo[6].angle = BLT + 10 + 35
-    kit.servo[7].angle = BLF + 10 + 12
+    kit.servo[6].angle = BLT + 10 + 40
+    kit.servo[7].angle = BLF + 10 + 15
 
-    kit.servo[9].angle = BRT - 10 - 35
-    kit.servo[10].angle = BRF - 10 - 12
+    kit.servo[9].angle = BRT - 10 - 40
+    kit.servo[10].angle = BRF - 10 - 15
 
 
 
