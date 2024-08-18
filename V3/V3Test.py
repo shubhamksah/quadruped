99645d9f65016618da3d80bb040d65e380afadd3
Shubham Sah <shubham.lal.sah@gmail.com>
2024-08-17 20:44:48 -0500

    f

--- V3/V3Test.py
+++ V3/V3Test.py
@@ -54,25 +54,25 @@ def main():
 
     time.sleep(1)
 
-    pos1move = Pos2FIncrement
-    pos2move = Pos2TIncrement
+    # pos1move = Pos2FIncrement
+    # pos2move = Pos2TIncrement
 
-    while pos2move <= Pos2T: 
-        kit.servo[1].angle = FLF + pos1move
-        kit.servo[4].angle = FRF - pos1move
-        kit.servo[7].angle = BLF + pos1move
-        kit.servo[10].angle = BRF - pos1move
-        pos1move += Pos2FIncrement
-        kit.servo[0].angle = FLT + pos2move
-        kit.servo[3].angle = FRT - pos2move
-        kit.servo[6].angle = BLT + pos2move
-        kit.servo[9].angle = BRT - pos2move
-        pos2move += Pos2TIncrement
-        kit.servo[2].angle = FLH
-        kit.servo[5].angle = FRH
-        kit.servo[8].angle = BLH
-        kit.servo[11].angle = BRH  
-        time.sleep(Pos2delay)
+    # while pos2move <= Pos2T: 
+    #     kit.servo[1].angle = FLF + pos1move
+    #     kit.servo[4].angle = FRF - pos1move
+    #     kit.servo[7].angle = BLF + pos1move
+    #     kit.servo[10].angle = BRF - pos1move
+    #     pos1move += Pos2FIncrement
+    #     kit.servo[0].angle = FLT + pos2move
+    #     kit.servo[3].angle = FRT - pos2move
+    #     kit.servo[6].angle = BLT + pos2move
+    #     kit.servo[9].angle = BRT - pos2move
+    #     pos2move += Pos2TIncrement
+    #     kit.servo[2].angle = FLH
+    #     kit.servo[5].angle = FRH
+    #     kit.servo[8].angle = BLH
+    #     kit.servo[11].angle = BRH  
+    #     time.sleep(Pos2delay)
 
     time.sleep(1)
 
