583a1f68e9fb1686216b932b73cd7f8498f58d47
Shubham Sah <shubham.lal.sah@gmail.com>
2024-08-17 22:00:37 -0500

    f

--- V3/V3walking.py
+++ V3/V3walking.py
@@ -74,20 +74,7 @@ kit = ServoKit(channels=16) #Initializes Servos
 
 def main():
 
-    t = 0
-
-    kit.servo[0].angle = FLT
-    kit.servo[1].angle = FLF
-    kit.servo[2].angle = FLH
-    kit.servo[3].angle = FRT
-    kit.servo[4].angle = FRF
-    kit.servo[5].angle = FRH
-    kit.servo[6].angle = BLT
-    kit.servo[7].angle = BLF
-    kit.servo[8].angle = BLH
-    kit.servo[9].angle = BRT
-    kit.servo[10].angle = BRF
-    kit.servo[11].angle = BRH  
+    t = 0 
 
     time.sleep(1)
 
