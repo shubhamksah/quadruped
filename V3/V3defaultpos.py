7a048f7b4e412734bdd3fb2f41e90c3107cb571f
Shubham Sah <shubham.lal.sah@gmail.com>
2024-09-01 20:54:26 -0500

    f

--- V3/V3defaultpos.py
+++ V3/V3defaultpos.py
@@ -39,6 +39,26 @@ try:
     kit.servo[9].angle = BRT
     kit.servo[10].angle = BRF
 
+    time.sleep(2)
+
+    kit.servo[2].angle = FLH
+    kit.servo[5].angle = FRH
+    kit.servo[8].angle = BLH
+    kit.servo[11].angle = BRH 
+
+    kit.servo[0].angle = FLT - 30
+    kit.servo[1].angle = FLF
+
+    kit.servo[3].angle = FRT - 30
+    kit.servo[4].angle = FRF
+
+    kit.servo[6].angle = BLT + 30
+    kit.servo[7].angle = BLF
+
+    kit.servo[9].angle = BRT + 30 
+    kit.servo[10].angle = BRF
+
+
 except:
     print("Exit")
     quit()
