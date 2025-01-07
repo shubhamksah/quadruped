a644b3591a6a1a399bedb9a4380537caf9f05c7c
Shubham Sah <shubham.lal.sah@gmail.com>
2025-01-06 20:24:39 -0600

    f

--- V3/RestPos.py
+++ V3/RestPos.py
@@ -20,9 +20,26 @@ BLF = rest
 BRT = rest
 BRF = rest
 
-def main():
+try:
 
-    print("Hello")
+    kit.servo[2].angle = FLH
+    kit.servo[5].angle = FRH
+    kit.servo[8].angle = BLH
+    kit.servo[11].angle = BRH 
 
-if __name__ == "__main__":
-    main()
+    kit.servo[0].angle = FLT
+    kit.servo[1].angle = FLF
+
+    kit.servo[3].angle = FRT
+    kit.servo[4].angle = FRF
+
+    kit.servo[6].angle = BLT
+    kit.servo[7].angle = BLF
+
+    kit.servo[9].angle = BRT
+    kit.servo[10].angle = BRF
+
+
+except:
+    print("Exit")
+    quit()
